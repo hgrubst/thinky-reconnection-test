dbe6158b2d6a9f6a6e158512dcf39cf16d348afa
Arthur Clement <aclement2@gmail.com>
2018-12-07 15:12:45 +1100

    reconnect on feed disconnect

--- User.ts
+++ User.ts
@@ -4,3 +4,42 @@ export const User = thinky.createModel('User', {
     firstName: String,
     lastName: String
 })
+
+
+export function listenToUserChanges() {
+    return User.changes().then(function (feed) {
+        console.log('Connected to change feeds');
+        feed.each(function (error, doc) {
+            if (error) {
+                console.log("Error while processing changefeed", error);
+                reconnect();
+            }
+
+            if (doc.isSaved() === false) {
+                console.log("The following document was deleted:");
+                console.log(doc);
+            }
+            else if (doc.getOldValue() == null) {
+                console.log("A new document was inserted:");
+                console.log((doc));
+            }
+            else {
+                console.log("A document was updated.");
+                console.log("Old value:");
+                console.log((doc.getOldValue()));
+                console.log("New value:");
+                console.log((doc));
+            }
+        });
+    }).error(function (error) {
+        console.log("Error while connecting to changefeed", error);
+        reconnect();
+    });
+}
+
+function reconnect() {
+    setTimeout(function () {
+        console.log('Trying to reconnect to db')
+        listenToUserChanges();
+    }, 5000)
+}
--- server.ts
+++ server.ts
@@ -1,14 +1,14 @@
 import { Server } from "hapi";
-import { User } from './User'
+import { User, listenToUserChanges } from './User'
 import thinky from './thinky';
 
 async function init() {
-    const server = new Server({port : 9000});
+    const server = new Server({ port: 9000 });
 
     server.route({
-        path : '/user', 
-        method : 'GET',
-        handler : async (request)=>{
+        path: '/user',
+        method: 'GET',
+        handler: async (request) => {
             let users = User.run();
             return users;
         }
@@ -17,32 +17,8 @@ async function init() {
     await server.start();
     console.log('Server started on port', server.info.port);
 
-    User.changes().then(function (feed) {
-        feed.each(function (error, doc) {
-            if (error) {
-                console.log("Error while processing changefeed", error);
-            }
+    listenToUserChanges();
 
-            if (doc.isSaved() === false) {
-                console.log("The following document was deleted:");
-                console.log(doc);
-            }
-            else if (doc.getOldValue() == null) {
-                console.log("A new document was inserted:");
-                console.log((doc));
-            }
-            else {
-                console.log("A document was updated.");
-                console.log("Old value:");
-                console.log((doc.getOldValue()));
-                console.log("New value:");
-                console.log((doc));
-            }
-        });
-    }).error(function (error) {
-        console.log("Error while connecting to changefeed", error);
-        // process.exit(1);
-    });
 
 }
 
